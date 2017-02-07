b0abd7d17b34bdcfa6849b48b1034fa56a16c439
rabbihossain <me@2hr.me>
2017-02-08 03:37:46 +0600

    recreated permission log generation feature

---
+++ app/src/main/java/io/a_ware/a_ware/TimeModel.java
@@ -0,0 +1,8 @@
+package io.a_ware.a_ware;
+
+/**
+ * Created by rabbi on 2/7/17.
+ */
+
+public class TimeModel {
+}
---
+++ app/src/main/java/io/a_ware/a_ware/TinyDB.java
@@ -0,0 +1,8 @@
+package io.a_ware.a_ware;
+
+/**
+ * Created by rabbi on 1/22/17.
+ */
+
+public class TinyDB {
+}
